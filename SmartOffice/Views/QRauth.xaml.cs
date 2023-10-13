a8f6bbc9fa9a71822118161a048e456405608cb8
F1ower <Daniil.shirkin005@gmail.com>
2023-10-14 02:57:46 +0300

    fixed

--- SmartOffice/Views/QRauth.xaml.cs
+++ SmartOffice/Views/QRauth.xaml.cs
@@ -17,37 +17,9 @@ namespace SmartOffice.Views
         {
             InitializeComponent();
         }
-
-
-        public ZXingBarcodeImageView GenerateQRCode(string userId)
-        {
-            var currentTime = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ");
-            var content = $"UserID:{userId}|Time:{currentTime}";
-
-            var barcode = new ZXingBarcodeImageView
-            {
-                HorizontalOptions = Xamarin.Forms.LayoutOptions.FillAndExpand,
-                VerticalOptions = Xamarin.Forms.LayoutOptions.FillAndExpand,
-            };
-
-
-        }
         private async void OnMain(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new MainPage());
-
-            barcode.BarcodeFormat = ZXing.BarcodeFormat.QR_CODE;
-            barcode.BarcodeOptions.Width = 300;
-            barcode.BarcodeOptions.Height = 300;
-            barcode.BarcodeValue = content;
-
-            return barcode;
         }
-
-        protected override void OnAppearing()
-        {
-            qrCodeImage.Source = GenerateQRCode("1").Source;
-        }
-
     }
 }
