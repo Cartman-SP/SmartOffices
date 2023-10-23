9f6602cfa176c70c9885d9c6f1a6f742318dd5e9
Artem <artem@yugaev.ru>
2023-10-23 14:50:47 +0300

    Map section added. Pop-up frame added to Map Page

--- SmartOffice/Views/Location.xaml.cs
+++ SmartOffice/Views/Location.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Rg.Plugins.Popup.Pages;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,13 @@ namespace SmartOffice.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Location : ContentPage
     {
+        private double maxY = 0;
         public Location()
         {
             InitializeComponent();
+            var panGesture = new PanGestureRecognizer();
+            panGesture.PanUpdated += OnPanUpdated;
+            DraggableContainer.GestureRecognizers.Add(panGesture);
         }
 
         private async void OnMain(object sender, EventArgs e)
@@ -25,6 +29,31 @@ namespace SmartOffice.Views
         {
             await Navigation.PushAsync(new YeelightPage());
         }
-        
+        private void OnPanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            switch (e.StatusType)
+            {
+                case GestureStatus.Started:
+                    // Сохраняем начальное положение элемента
+                    maxY = DraggableContainer.TranslationY;
+                    break;
+                case GestureStatus.Running:
+                    // Обработка перетаскивания в реальном времени.
+                    double newY = maxY + e.TotalY;
+
+                    // Задаем максимальную длину перетаскивания
+                    double maxDragDistance = 300; // Измените на желаемое значение.
+
+                    // Проверяем, не превышает ли перемещение максимальное значение.
+                    if (Math.Abs(newY) <= maxDragDistance)
+                    {
+                        DraggableContainer.TranslationY = newY;
+                    }
+                    break;
+                case GestureStatus.Completed:
+                    // Действия, выполняемые после завершения перетаскивания (если требуется).
+                    break;
+            }
+        }
     }
 }
