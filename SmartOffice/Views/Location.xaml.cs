b5b7062f6b21cf2e22918dbe998e9723a1858832
Artem <artem@yugaev.ru>
2023-10-23 23:38:38 +0300

    Map pop-up changed

--- SmartOffice/Views/Location.xaml.cs
+++ SmartOffice/Views/Location.xaml.cs
@@ -12,13 +12,9 @@ namespace SmartOffice.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Location : ContentPage
     {
-        private double maxY = 0;
         public Location()
         {
             InitializeComponent();
-            var panGesture = new PanGestureRecognizer();
-            panGesture.PanUpdated += OnPanUpdated;
-            DraggableContainer.GestureRecognizers.Add(panGesture);
         }
 
         private async void OnMain(object sender, EventArgs e)
@@ -28,32 +24,6 @@ namespace SmartOffice.Views
         private async void OnLight(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new YeelightPage());
-        }
-        private void OnPanUpdated(object sender, PanUpdatedEventArgs e)
-        {
-            switch (e.StatusType)
-            {
-                case GestureStatus.Started:
-                    // Сохраняем начальное положение элемента
-                    maxY = DraggableContainer.TranslationY;
-                    break;
-                case GestureStatus.Running:
-                    // Обработка перетаскивания в реальном времени.
-                    double newY = maxY + e.TotalY;
-
-                    // Задаем максимальную длину перетаскивания
-                    double maxDragDistance = 300; // Измените на желаемое значение.
-
-                    // Проверяем, не превышает ли перемещение максимальное значение.
-                    if (Math.Abs(newY) <= maxDragDistance)
-                    {
-                        DraggableContainer.TranslationY = newY;
-                    }
-                    break;
-                case GestureStatus.Completed:
-                    // Действия, выполняемые после завершения перетаскивания (если требуется).
-                    break;
-            }
         }
     }
 }
