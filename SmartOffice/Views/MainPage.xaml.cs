4a4660400e675bc133180e02094249c05ddea413
Artem <artem@yugaev.ru>
2023-10-26 21:07:48 +0300

    bugs fixed

--- SmartOffice/Views/MainPage.xaml.cs
+++ SmartOffice/Views/MainPage.xaml.cs
@@ -4,20 +4,43 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
+using Microcharts;
+using SkiaSharp;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Entry = Microcharts.ChartEntry;
 
 namespace SmartOffice.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
+        List<Entry> entries = new List<Entry>
+        {
+            new Entry(10)
+            {
+                Color = SKColor.Parse("#498C8A"),
+                Label = "sample",
+            },
+            new Entry(20)
+            {
+                Color = SKColor.Parse("#498C8A"),
+                Label = "sample",
+            },
+            new Entry(5)
+            {
+                Color = SKColor.Parse("#498C8A"),
+                Label = "sample",
+            }
+        };
+
         public MainPage()
         {
             InitializeComponent();
 
-            NavigationPage.SetHasNavigationBar(this, false);
+            //NavigationPage.SetHasNavigationBar(this, false);
+            MainChart.Chart = new LineChart { Entries = entries };
 
         }
         private async void OnNotify(object sender, EventArgs e)
@@ -45,6 +68,4 @@ namespace SmartOffice.Views
             await Navigation.PushAsync(new Help());
         }
     }
-
-
 }
